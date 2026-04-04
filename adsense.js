ae4f30173f7f3acd09f79e42b425dd4175f3a0b2
anne777-collab <sahilsharma582005@gmail.com>
2026-04-04 16:48:38 +0530

    Add adsense code

---
+++ adsense.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6342407589181909";
+  script.crossOrigin = "anonymous";
+  document.head.appendChild(script);
+})();
